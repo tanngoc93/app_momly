2fa495cfc0bf3940df66cb6a3c7e464a99fded5d
Ngoc Nguyen <ngoc.nguyen@oivan.com>
2025-05-12 23:11:42 +0700

    Fix bugs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || root_path
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
