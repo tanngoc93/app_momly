470bfc7e53c1f337588ef773ba2779ffbf9ed621
Ngoc Nguyen Tan <4559175+tanngoc93@users.noreply.github.com>
2025-06-10 13:00:41 +0700

    Filter public links to web source

--- test/controllers/public_links_controller_test.rb
+++ test/controllers/public_links_controller_test.rb
@@ -1,16 +1,19 @@
 require "test_helper"
 
 class PublicLinksControllerTest < ActionDispatch::IntegrationTest
-  test "should list only publicly visible guest links" do
-    ShortLink.create!(original_url: "https://example.com", short_code: "abc123", publicly_visible: true, user_id: nil)
+  test "should list only publicly visible links created via web" do
     user = User.create!(email: "user@example.com", password: "password")
-    ShortLink.create!(original_url: "https://member.com", short_code: "mem001", publicly_visible: true, user: user)
-    ShortLink.create!(original_url: "https://hidden.com", short_code: "zzz999", publicly_visible: false, user_id: nil)
+
+    ShortLink.create!(original_url: "https://webuser.com", short_code: "web001", publicly_visible: true, user: user, source: :web)
+    ShortLink.create!(original_url: "https://legacy.com", short_code: "legacy01", publicly_visible: true)
+    ShortLink.create!(original_url: "https://api.com", short_code: "api001", publicly_visible: true, source: :api)
+    ShortLink.create!(original_url: "https://hidden.com", short_code: "hidden01", publicly_visible: false, source: :web)
 
     get public_links_url
     assert_response :success
-    assert_includes @response.body, "abc123"
-    refute_includes @response.body, "mem001"
-    refute_includes @response.body, "zzz999"
+    assert_includes @response.body, "web001"
+    assert_includes @response.body, "legacy01"
+    refute_includes @response.body, "api001"
+    refute_includes @response.body, "hidden01"
   end
 end
