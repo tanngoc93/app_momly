ed9ec75f97d11f853f650999c808491965839a3b
Ngoc Nguyen Tan <4559175+tanngoc93@users.noreply.github.com>
2025-06-11 07:54:03 +0700

    Show QR modal on submit and validate URL

--- app/controllers/qr_codes_controller.rb
+++ app/controllers/qr_codes_controller.rb
@@ -3,15 +3,23 @@ class QrCodesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def create
-    @url = params[:url].to_s.strip
-    if @url.blank? || !@url.match?(/\Ahttps?:\/\//)
-      @error = "Invalid URL"
-      render partial: "qr_codes/form", status: :unprocessable_entity
-    else
+    @url = params[:url].to_s
+
+    begin
+      @url = normalize_url(@url)
+      raise ShortLinkServices::BlockedDomainError, "You cannot shorten a Momly link" if own_domain?(@url)
+
+      unless GoogleSafeBrowsingService.safe_url?(@url)
+        raise ShortLinkServices::UnsafeUrlError, "URL is unsafe or unreachable"
+      end
+
       respond_to do |format|
         format.turbo_stream
         format.html { render :create }
       end
+    rescue ArgumentError, ShortLinkServices::BlockedDomainError, ShortLinkServices::UnsafeUrlError => e
+      @error = e.message
+      render partial: "qr_codes/form", status: :unprocessable_entity
     end
   end
 
@@ -24,5 +32,28 @@ class QrCodesController < ApplicationController
     else
       send_data png.to_s, type: "image/png", disposition: "inline"
     end
+  end
+
+  private
+
+  def normalize_url(url)
+    stripped = url.to_s.strip
+    raise ArgumentError, "URL can't be blank" if stripped.blank?
+
+    uri = URI.parse(stripped)
+    unless uri.scheme&.match?(/^https?$/)
+      uri = URI.parse("https://#{stripped}")
+    end
+
+    uri.to_s
+  rescue URI::InvalidURIError
+    raise ArgumentError, "Invalid URL format"
+  end
+
+  def own_domain?(url)
+    uri = URI.parse(url)
+    host = uri.host.to_s.downcase
+    domains = Rails.application.config.momly_domains
+    domains.any? { |domain| host == domain || host.end_with?(".#{domain}") }
   end
 end
--- test/controllers/qr_codes_controller_test.rb
+++ test/controllers/qr_codes_controller_test.rb
@@ -2,10 +2,17 @@ require "test_helper"
 
 class QrCodesControllerTest < ActionDispatch::IntegrationTest
   test "generate and download qr" do
-    post qr_code_url, params: { url: "https://example.com" }
-    assert_response :success
+    GoogleSafeBrowsingService.stub(:safe_url?, true) do
+      post qr_code_url, params: { url: "https://example.com" }
+      assert_response :success
+    end
 
     get qr_code_url(url: "https://example.com")
     assert_equal "image/png", @response.media_type
+  end
+
+  test "invalid url returns unprocessable entity" do
+    post qr_code_url, params: { url: "bad" }
+    assert_response :unprocessable_entity
   end
 end
