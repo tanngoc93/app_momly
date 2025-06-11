25c1a1192459624067d3002460347a79fdc0cbdb
Ngoc Nguyen Tan <4559175+tanngoc93@users.noreply.github.com>
2025-06-12 00:04:07 +0700

    Fix page view and controller to resolve conflicts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,8 +6,5 @@ class PagesController < ApplicationController
     raise ActiveRecord::RecordNotFound unless slug.match?(Page::SLUG_FORMAT)
 
     @page = Page.find_by!(slug: slug)
-
-    content_for :page_title, @page.meta_title.presence || @page.title
-    content_for :meta_description, @page.meta_description if @page.meta_description.present?
   end
 end
