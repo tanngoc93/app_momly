7d71e5a5ab38761ec1899369133687d0fc46caa9
Ngoc Nguyen Tan <4559175+tanngoc93@users.noreply.github.com>
2025-06-11 22:30:23 +0700

    Remove unnecessary JavaScript comments

--- app/javascript/controllers/tooltip_controller.js
+++ app/javascript/controllers/tooltip_controller.js
@@ -1,7 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 import { Tooltip } from "bootstrap"
 
-// Connects to data-controller="tooltip"
 export default class extends Controller {
   connect() {
     this.tooltip = new Tooltip(this.element)
