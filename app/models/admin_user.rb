76db02688d8cdb7cf6e72f692603bc1127e7becf
Ngoc Nguyen <ngoc.nguyen@oivan.com>
2025-06-11 12:34:58 +0700

    Update app/models/admin_user.rb

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,12 +1,6 @@
 class AdminUser < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, 
-         :recoverable, :rememberable, :validatable
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, 
-         :recoverable, :rememberable, :validatable
+  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :recoverable, :rememberable,
          :validatable, :trackable
 
